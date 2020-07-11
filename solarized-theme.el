5842362abdbe5130c65b89909d8bfdb551d0b77b
James Porter <james@jamesporter.me>
2020-07-11 15:52:41 -0400

    remove unnecessary provide

--- solarized-theme.el
+++ solarized-theme.el
@@ -2,6 +2,8 @@
          (locate-file "solarized-definitions.el" custom-theme-load-path
                       '("c" "")))
 
+;; we don't need a provide in this file becuase this macro calls
+;; `provide-theme' internally
 (create-solarized-theme solarized
                         solarized-description (solarized-color-definitions))
 
@@ -9,5 +11,3 @@
 (when (and (boundp 'custom-theme-load-path) load-file-name)
   (add-to-list 'custom-theme-load-path
                (file-name-as-directory (file-name-directory load-file-name))))
-
-(provide 'solarized)
