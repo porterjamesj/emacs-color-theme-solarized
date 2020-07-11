c7492af862771207d529cc62339fb6ce49812edd
James Porter <james@jamesporter.me>
2020-07-11 15:25:31 -0400

    remove stuff, add things ripped from bbatsov solarized

--- solarized-theme.el
+++ solarized-theme.el
@@ -4,3 +4,10 @@
 
 (create-solarized-theme solarized
                         solarized-description (solarized-color-definitions))
+
+;;;###autoload
+(when (and (boundp 'custom-theme-load-path) load-file-name)
+  (add-to-list 'custom-theme-load-path
+               (file-name-as-directory (file-name-directory load-file-name))))
+
+(provide 'solarized)
